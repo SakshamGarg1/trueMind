ada9d3c9dce7961cfdf4d2b477dd7202b9999e19
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2022-07-30 12:26:07 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <>
+        <footer>
+          <div className="footer">
+            <p className="myfontt">
+              Made by : 
+              <span className="myfont" > Saksham Garg</span>
+            </p>
+          </div>
+        </footer>
+      </>
+    );
+  }
+  export default Footer;
